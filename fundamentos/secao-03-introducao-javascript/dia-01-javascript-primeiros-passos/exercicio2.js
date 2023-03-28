27bb9db77c944375794270d74b76eb73a4e7e056
Rodrigo Beckmann <rodrigodsbeckmann@gmail.com>
2023-03-28 18:28:54 -0300

    inicio exercicios dia 1 de introducao a javascript

---
+++ fundamentos/secao-03-introducao-javascript/dia-01-javascript-primeiros-passos/exercicio2.js
@@ -0,0 +1,10 @@
+let a = 5;
+let b = 10;
+
+if (a > b) {
+    console.log(a);
+} else if (b > a) {
+    console.log(b);
+} else if (a === b) {
+    console.log('os valores são iguais');
+} 
---
+++ fundamentos/secao-03-introducao-javascript/dia-01-javascript-primeiros-passos/exercicio3.js
@@ -0,0 +1,11 @@
+let a = 3;
+let b = 12;
+let c = 10;
+
+if (a > b && a > c) {
+    console.log(a);
+} else if (b > a && b > c) {
+    console.log(b);
+} else if (c > a && c > b) {
+    console.log (c);
+}
