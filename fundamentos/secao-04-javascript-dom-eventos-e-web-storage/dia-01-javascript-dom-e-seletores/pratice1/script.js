987089af033ab00a6b85596463c78a0bbc841614
Rodrigo Beckmann <rodrigodsbeckmann@gmail.com>
2023-04-10 16:02:27 -0300

    criação pasta dia 1 DOM

---
+++ fundamentos/secao-04-javascript-dom-eventos-e-web-storage/dia-01-javascript-dom-e-seletores/pratice1/script.js
@@ -0,0 +1,8 @@
+const paragraph = document.getElementById("paragraph");
+paragraph.style.color = "red";
+
+document.getElementById('page-title').innerText = 'The Godfather'
+
+document.getElementById('second-paragraph').innerText = 'No verão de 1945, Don Vito Corleone ouve pedidos de favores durante o casamento da sua filha Connie, enquanto o seu consigliere e filho adotivo, Tom Hagen, apenas escuta. O cantor e afilhado do Don, Johnny Fontane, pede ajuda para estrelar um filme que vai ajudá-lo a reerguer sua carreira. Hagen vai à Califórnia para se encontrar com o chefe do estúdio Jack Woltz com o objetivo de adquirir o papel para Fontane; depois de recusar-se a escalar Fontane, Woltz desperta na manhã seguinte com a cabeça decapitada do seu valioso e estimado cavalo Khartum na cama.'
+
+document.getElementById('subtitle').innerText = 'Enredo'
