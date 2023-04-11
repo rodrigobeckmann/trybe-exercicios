edcb4baf3b22c698d152c92e02ff62983bb31ce9
Rodrigo Beckmann <rodrigodsbeckmann@gmail.com>
2023-04-11 15:44:16 -0300

    exercicios de pratica dia 2 secao 4

--- fundamentos/secao-04-javascript-dom-eventos-e-web-storage/dia-02-javascript-trabalhando-com-elementos-aberto-item-de-menu-com-subitens/script.js
+++ fundamentos/secao-04-javascript-dom-eventos-e-web-storage/dia-02-javascript-trabalhando-com-elementos-aberto-item-de-menu-com-subitens/script.js
@@ -1,11 +1,4 @@
 const whereAreYou = document.getElementById('where-are-you');
-const firstChildOfChild = document.getElementById('first-child-of-child');
-const firstChild = document.querySelector('#parent').firstElementChild;
-const firstChildWhere = whereAreYou.previousElementSibling;
-const attention = whereAreYou.nextSibling;
-const thirdChild = whereAreYou.nextElementSibling;
-const thirdChildParent = document.querySelector('#parent').lastElementChild.previousElementSibling;
+const h2WhereAreYou = document.querySelector('#where-are-you h2');
 
-whereAreYou.parentNode.style.color = 'red';
-
-firstChildOfChild.innerText = 'Exercicio trybe';
+whereAreYou.removeChild(h2WhereAreYou);
